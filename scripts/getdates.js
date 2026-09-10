362266d0c8f938b54c2ae569fdf864d416282eb0
alan-technology <felizalan8@gmail.com>
2026-09-09 22:59:11 -0400

    new style

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Año actual dinámico
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Última fecha de modificación del documento
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
